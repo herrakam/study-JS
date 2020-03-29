b1b4f9bed413f6b330152e48da26e042de6ecba9
정승현 <fm1003@naver.com>
2020-03-29 17:10:59 +0900

    clock

---
+++ clock.js
@@ -0,0 +1,15 @@
+const clockContainer = document.querySelector(".js-clock");
+const clock=clockContainer.querySelector("h1");
+
+function getTime(){
+    const time = new Date();
+    const hour = time.getHours();
+    const minute = time.getMinutes();
+    const second = time.getSeconds();
+    clock.innerText = `${hour <10 ?  `0${hour}` : hour}:${minute <10 ? `0${minute}` : minute}:${second<10 ?  `0${second}` : second}`;
+}
+function init(){
+    setInterval(getTime,1000); // 1000ms 당 한번씩 getTime을 실행해라
+}
+
+init();
